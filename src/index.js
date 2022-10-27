c6a39fd8b1254cdaa4f6a48daf8025d94449ae5b
Fnwrkio <firmansyah.assignment@gmail.com>
2022-10-28 00:19:39 +0700

    feat: add authentication

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
-const { PrismaClient } = require('@prisma/client');
-const { ApolloServer } = require('apollo-server');
-
 const fs = require('fs');
 const path = require('path');
 
+const { PrismaClient } = require('@prisma/client');
+const { ApolloServer } = require('apollo-server');
+
+const { getUserId } = require('./util');
+
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+
 const resolvers = {
-  Query: {
-    info: () => 'This is the API of a backend',
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany();
-    }
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description
-        }
-      });
-      return newLink;
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 const prisma = new PrismaClient();
@@ -29,8 +23,12 @@ const prisma = new PrismaClient();
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
-  context: {
-    prisma
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null
+    };
   }
 });
 
