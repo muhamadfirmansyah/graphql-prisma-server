3112b84c10cec08665ac78086e9e2e182a775e7b
Fnwrkio <firmansyah.assignment@gmail.com>
2022-10-27 22:33:37 +0700

    adding mutation add and delete

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
 const { ApolloServer } = require('apollo-server');
 
-/**
- * The typeDefs constant defines your GraphQL schema (more about this in a bit). Here, it defines a
- * simple Query type with one field called info. This field has the type String!. The exclamation
- * mark in the type definition means that this field is required and can never be null.
- */
-const typeDefs = `
-    type Query{
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Mutation {
-        post(url: String!, description: String!): Link!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`;
-
-/**
- * The links variable is used to store the links at
- * runtime. For now, everything is stored only in-memory
- * rather than being persisted in a database.
- */
 let links = [
   {
     id: 'link-0',
@@ -35,42 +11,37 @@ let links = [
   }
 ];
 
-/**
- * The resolvers object is the actual implementation of the GraphQL schema. Notice how its structure
- * is identical to the structure of the type definition inside typeDefs: Query.info.
- */
-
 const resolvers = {
   Query: {
     info: () => 'This is the API of a backend',
-    /**
-     * You are adding a new resolver for the feed root
-     * field. Notice that a resolver always has to be
-     * named exactly after the corresponding field from
-     * the schema definition.
-     */
     feed: () => links
   },
-  /**
-   * Finally, you are adding three more resolvers for the
-   * fields on the Link type from the schema definition.
-   * We willl discuss what the parent argument that is
-   * passed into the resolver here is in a bit.
-   */
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+
+      links.push(link);
+
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      let id = args.id;
+
+      links = links.filter((item) => item.id != id);
+
+      return
+    }
   }
 };
 
-/**
- * Finally, the schema and resolvers are bundled and passed to ApolloServer which is imported from
- * apollo-server. This tells the server what API operations are accepted and how they should be
- * resolved.
- */
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers
 });
 
