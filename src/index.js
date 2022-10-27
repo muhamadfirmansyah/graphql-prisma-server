123b68ba81bded08c6b2221eca28a43f8f7641f7
Fnwrkio <firmansyah.assignment@gmail.com>
2022-10-27 23:09:43 +0700

    feat: connect db to current server and display prisma studio

--- src/index.js
+++ src/index.js
@@ -1,48 +1,37 @@
-const fs = require('fs');
-const path = require('path');
-
+const { PrismaClient } = require('@prisma/client');
 const { ApolloServer } = require('apollo-server');
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'google.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-];
+const fs = require('fs');
+const path = require('path');
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a backend',
-    feed: () => links
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-
-      links.push(link);
-
-      return link;
-    },
-    deleteLink: (parent, args) => {
-      let id = args.id;
-
-      links = links.filter((item) => item.id != id);
-
-      return
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      });
+      return newLink;
     }
   }
 };
 
+const prisma = new PrismaClient();
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
